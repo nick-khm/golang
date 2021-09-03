46284ef1e6cda3e596af8640be453545062413cc
Nick Kharaim <nick.kharaim@actility.com>
2021-09-03 09:28:20 +0200

    declare variable in if condition

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -8,4 +8,10 @@ import (
 
 func main() {
 	fmt.Println(data.Name, data.Age)
+
+	if count := 12; count > 10 {
+		fmt.Printf("Counter is enough, = %d\n", count)
+	} else {
+		fmt.Printf("Counter is not enough, = %d\n", count)
+	}
 }
