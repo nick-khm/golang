43b892f9ead44e7d1a74a128ded6d5b40398bfa8
Nick Kharaim <nick.kharaim@actility.com>
2021-09-03 09:23:07 +0200

    Variable accessibility

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -2,22 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"helloworld.com/greetings/src/training.go/helloworld/data"
 )
 
-var pi = 3.14
-
 func main() {
-	var age int = 20
-	fmt.Println(age)
-
-	var weight, height int = 80, 190
-	fmt.Println(weight, height)
-
-	sex, politic := "male", "respublican"
-	fmt.Println(sex, politic)
-
-	kids, car := 0, "pontiac"
-	fmt.Println(kids, car)
-
-	fmt.Println(pi)
+	fmt.Println(data.Name, data.Age)
 }
