816bebd278bbf2cc5b6adb6273c285cbf05c81b2
Nick Kharaim <nick.kharaim@actility.com>
2021-09-04 23:01:50 +0200

    Functions

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -4,15 +4,30 @@ import (
 	"fmt"
 )
 
+func printInfoParams(name string, age int, email string) {
+	fmt.Printf("Name=%s, age=%d, email=%s\n", name, age, email)
+}
+
+func avg(x, y float64) float64 {
+	return (x + y) / 2
+}
+
+func sumNamedReturn(x, y, z int) (sum int) {
+	sum = x + y + z
+	return sum
+}
+
+func sumNamedReturn2(x, y, z int) (sum int) {
+	sum = x + y + z
+	return
+}
+
 func main() {
-	var percentage float64 = 2.5
-
-	percentage = percentage + 25
-
-	fmt.Printf("Percentage %f%%\n", percentage)
-	fmt.Printf("Int value %d%%\n", int(percentage))
-
-	n := 42
-	f := float64(n) + 0.42
-	fmt.Printf("float=%f\n", f)
+	printInfoParams("Bob", 20, "bob@t.com")
+	avgResult := avg(32, 34.3)
+	fmt.Printf("AVG result=%f\n", avgResult)
+	sum := sumNamedReturn(8, 32, 5)
+	fmt.Printf("Sum result=%d\n", sum)
+	sum2 := sumNamedReturn2(8, 32, 5)
+	fmt.Printf("Sum result=%d\n", sum2)
 }
