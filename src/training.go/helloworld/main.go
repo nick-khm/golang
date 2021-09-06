70f380d11966ba208bba7d0ea562f0389b629493
Nick Kharaim <nick.kharaim@actility.com>
2021-09-06 21:56:24 +0200

    Handle errors

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 )
 
-func main() {
-	names := []string{"Bob", "Alice", "Bobette", "John"}
-	for i, name := range names {
-		fmt.Printf("Name=%s, index=%d\n", name, i)
+func readFile(filename string) (string, error) {
+	dat, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
 	}
+	if len(dat) == 0 {
+		// return "", errors.New("Empty content")
+		return "", fmt.Errorf("empty content (filename=%v)", filename)
+	}
+
+	return string(dat), nil
+}
+
+func main() {
+	dat, err := readFile("test.txt")
+	if err != nil {
+		fmt.Printf("Error while reading file: %v\n", err)
+		return
+	}
+	fmt.Println("File content:")
+	fmt.Println(dat)
 }
