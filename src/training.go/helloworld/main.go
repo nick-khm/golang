73ac72944cb0ee288e6b3f78e3af4c224abdf691
Nick Kharaim <nick.kharaim@actility.com>
2021-09-06 13:29:56 +0200

    Slices

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -10,15 +10,42 @@ func ToLowerStr(name string) (string, int) {
 }
 
 func main() {
+	nums := make([]int, 2, 3)
+	nums[0] = 23
+	nums[1] = 64
+	fmt.Printf("%v (len=%d, cap=%d)\n", nums, len(nums), cap(nums))
+	nums = append(nums, 8)
+	fmt.Printf("%v (len=%d, cap=%d)\n", nums, len(nums), cap(nums))
+	// will double a capacity
+	nums = append(nums, 99)
+	fmt.Printf("%v (len=%d, cap=%d)\n", nums, len(nums), cap(nums))
 
-	var names [3]string
-	fmt.Printf("names=%v (len=%v)\n", names, len(names))
+	// dynamic slice
+	letters := []string{"g", "o", "l", "a", "n", "g"}
+	fmt.Printf("letters %v (len=%d, cap=%d)\n", letters, len(letters), cap(letters))
+	fmt.Println("-----------------------------")
 
-	names[0] = "Bob"
-	names[2] = "Alice"
+	// sub slice
+	sub1 := letters[0:2] // or letters [:2]
+	sub2 := letters[2:]  // will take untile the end
+	fmt.Printf("sub1 %v (len=%d, cap=%d)\n", sub1, len(sub1), cap(sub1))
+	fmt.Printf("sub2 %v (len=%d, cap=%d)\n", sub2, len(sub2), cap(sub2))
+	fmt.Println("-----------------------------")
 
-	fmt.Printf("names=%v (len=%v)\n", names, len(names))
+	// slice changeing will impact the source array
+	sub2[0] = "YOLO"
+	fmt.Printf("sub1 %v (len=%d, cap=%d)\n", sub1, len(sub1), cap(sub1))
+	fmt.Printf("sub2 %v (len=%d, cap=%d)\n", sub2, len(sub2), cap(sub2))
+	fmt.Printf("letters %v (len=%d, cap=%d)\n", letters, len(letters), cap(letters))
+	fmt.Println("-----------------------------")
 
-	odds := [4]int{44, 8, 32}
-	fmt.Printf("odds=%v (len=%d)\n", odds, len(odds))
+	// copy a part of an array, not referenced
+	subCopy := make([]string, len(sub1))
+	copy(subCopy, sub1)
+	subCopy[1] = "WIN"
+	fmt.Printf("sub1 %v (len=%d, cap=%d)\n", sub1, len(sub1), cap(sub1))
+	fmt.Printf("sub2 %v (len=%d, cap=%d)\n", sub2, len(sub2), cap(sub2))
+	fmt.Printf("subCopy %v (len=%d, cap=%d)\n", subCopy, len(subCopy), cap(subCopy))
+	fmt.Printf("letters %v (len=%d, cap=%d)\n", letters, len(letters), cap(letters))
+	fmt.Println("-----------------------------")
 }
