b1fd1b8d8755ac7c86c0155a08d2b8f1f7a29117
Nick Kharaim <nick.kharaim@actility.com>
2021-09-06 21:44:35 +0200

    Range

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -2,25 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func ToLowerStr(name string) (string, int) {
-	return strings.ToLower(name), len(name)
-}
-
 func main() {
-	i := 0
-	for {
-		i++
-		if i%2 != 0 {
-			fmt.Println("Odd looping")
-			continue
-		}
-		fmt.Println("Looping")
-		if i >= 10 {
-			fmt.Println("Exit looping")
-			break
-		}
+	names := []string{"Bob", "Alice", "Bobette", "John"}
+	for i, name := range names {
+		fmt.Printf("Name=%s, index=%d\n", name, i)
 	}
 }
