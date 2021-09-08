1d135db363f425d07c6a3db2f661c416f10bfb9f
Nick Kharaim <nick.kharaim@actility.com>
2021-09-08 22:13:11 +0200

    FindAndReplace correction

--- src/training.go/helloworld/main.go
+++ src/training.go/helloworld/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -19,7 +21,46 @@ func ProcessLine(line, old, new string) (found bool, res string, occ int) {
 	return found, res, occ
 }
 
+func FindAndReplace(srcFile, old, new string) (occ int, lines []int, err error) {
+	file, err := os.Open(srcFile)
+	if err != nil {
+		return occ, lines, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	lineIdx := 1
+	for scanner.Scan() {
+		found, res, o := ProcessLine(scanner.Text(), old, new)
+		if found {
+			occ += o
+			lines = append(lines, lineIdx)
+		}
+		fmt.Println(res)
+		lineIdx++
+	}
+	return occ, lines, nil
+}
+
 func main() {
-	found, res, occ := ProcessLine("Go is the best language to learn. You should go to the official website.", "Go", "Python")
-	fmt.Println(found, res, occ)
+	old := "Go"
+	new := "Python"
+	occ, lines, err := FindAndReplace("wikigo.txt", old, new)
+	if err != nil {
+		fmt.Printf("Error while executing find replace: %v\n", err)
+	}
+	fmt.Println("=== Summary ===")
+	defer fmt.Println("=== END Summary ===")
+	fmt.Printf("Number of occurences of %v: %v\n", old, occ)
+	fmt.Printf("Number of lines: %d\n", len(lines))
+
+	len := len(lines)
+	fmt.Print("Lines [ ")
+	for i, l := range lines {
+		fmt.Print(l)
+		if i < len-1 {
+			fmt.Print(" - ")
+		}
+	}
+	fmt.Println(" ]")
 }
